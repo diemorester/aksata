3cecd6d502e1dd2a0fb46c39dadc30735d3285e5
diemorester <heimrband@gmail.com>
2024-12-16 14:55:47 +0700

    feat: push push

--- apps/web/src/libs/fetch/absensi.ts
+++ apps/web/src/libs/fetch/absensi.ts
@@ -1,4 +1,3 @@
-import { AbsensiResponse } from '@/types/absensiTypes';
 import axiosInstance from '../axios';
 import { getCookie } from '../server';
 
@@ -32,16 +31,4 @@ export const clockOutFetch = async () => {
   );
 
   return res;
-};
-
-export const fetchAbsensi = async ( userId: number, page: number, limit: number, search?: string): Promise<AbsensiResponse> => {
-  const res = await axiosInstance.get<AbsensiResponse>("/getall-attendace", {
-    params: {
-      userId,
-      page,
-      limit,
-      ...(search && { search })
-    }
-  });
-  return res.data
 };
