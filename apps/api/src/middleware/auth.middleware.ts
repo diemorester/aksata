efe4e0a7e952db5472a6028e18d030373d3b617e
Dietyy <radietdesfandri@gmail.com>
2024-12-16 11:03:51 +0700

    Absensi (#12)
    * feat: added be absensi, clockin and clockout
    
    * feat: absensi done
    
    * feat: added get all attendace

--- apps/api/src/middleware/auth.middleware.ts
+++ apps/api/src/middleware/auth.middleware.ts
@@ -7,9 +7,11 @@ export const authMiddleware = async (
   next: NextFunction,
 ) => {
   try {
-    const token = req.cookies?.accessToken || req.headers.authorization?.replace('Bearer ', '');
+    const token =
+      req.cookies?.accessToken ||
+      req.headers.authorization?.replace('Bearer ', '');
     if (!token) throw 'Token empty';
-    
+
     const verifyUser = verify(token, process.env.SECRET_KEY!);
     req.user = verifyUser as User;
 
@@ -17,6 +19,30 @@ export const authMiddleware = async (
   } catch (error) {
     res.status(400).send({
       status: 'ERROR MIDDLEWARE',
+      msg: error,
+    });
+  }
+};
+
+export const accessAdminHr = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.headers.authorization?.replace('Bearer ', '');
+    const verifyUser = verify(token, process.env.SECRET_KEY!);
+    req.user = verifyUser as User;
+
+    if (req.user.role !== 'AdminHR' && req.user.role !== 'SuperAdmin')
+      throw 'only Admin HR and SuperAdmin';
+
+    next();
+  } catch (error) {
+    return res.status(400).send({
+      status: 'ERROR ACCESS ROLE',
       msg: error,
     });
   }
