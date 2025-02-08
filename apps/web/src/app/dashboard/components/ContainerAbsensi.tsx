ddab880667443cc5c51d95fff028d94b10dd49cb
RadidDesafandri <radietdesfandri@gmail.com>
2025-02-08 21:05:10 +0700

    feat: location and coordinat

--- apps/web/src/app/dashboard/components/ContainerAbsensi.tsx
+++ apps/web/src/app/dashboard/components/ContainerAbsensi.tsx
@@ -1,11 +1,17 @@
+'use client';
+
+import { BarChartComponent } from '@/components/ui/barChart';
 import { PieChartComponent } from '@/components/ui/pieChart';
 import Absensi from './Absensi';
 import AbsensiHistory from './AbsensiHistory';
 import ListApproval from './ListApproval';
+import LocationsHistory from './LocationsHistory';
 import PengajuanKonten from './PengajuanKonten';
-import { BarChartComponent } from '@/components/ui/barChart';
+import { useGeolocation } from '@/hooks/useGeolocation ';
+import Map from '@/components/Map';
 
 const ContainerAbsensi = () => {
+  const { location } = useGeolocation();
   return (
     <div className="grid grid-cols-1 md:grid-cols-8 w-full min-h-[100px] md:min-h-[550px] md:grid-rows-6 gap-4 h-full">
       <div className="md:col-span-2 md:row-span-3 bg-black rounded-xl p-3">
@@ -14,15 +20,14 @@ const ContainerAbsensi = () => {
       <div className="md:col-span-2 md:row-span-3 md:col-start-7 md:row-start-4 bg-black rounded-xl p-3">
         <Absensi />
       </div>
-      <div className="flex md:flex-row flex-col-reverse justify-between md:col-span-4 md:row-span-3 md:col-start-3 md:row-start-4 bg-black rounded-xl p-3">
-        <p>HISTORY</p>
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15843.223187511981!2d107.5904121994972!3d-6.913808456865164!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e727f9c63d7d%3A0xfcdaa626835e74fa!2sStasiun%20Bandung!5e0!3m2!1sid!2sid!4v1738749489171!5m2!1sid!2sid"
-          className="rounded-xl h-full"
-        ></iframe>
+      <div className="flex md:flex-row flex-col-reverse gap-3 justify-between md:col-span-4 md:row-span-3 md:col-start-3 md:row-start-4 bg-black rounded-xl p-3">
+        <LocationsHistory />
       </div>
       <div className="md:col-span-2 md:row-span-3 md:col-start-1 md:row-start-4 bg-black rounded-xl p-3">
-        <BarChartComponent />
+        {/* <BarChartComponent /> */}
+        {location && (
+          <Map latitude={location?.latitude} longitude={location?.longitude} />
+        )}
       </div>
       <div className="justify-center flex items-center md:col-span-2 md:row-span-3 md:col-start-7 md:row-start-1 bg-black rounded-xl p-3">
         <AbsensiHistory />
